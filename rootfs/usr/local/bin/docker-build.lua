00baa60d6610074a18cf0bacf6c4df93f7ce83e3
agrozyme <agrozyme@gmail.com>
2019-10-21 15:47:18 +0800

    updat

--- rootfs/usr/local/bin/docker-build.lua
+++ rootfs/usr/local/bin/docker-build.lua
@@ -2,9 +2,11 @@
 local core = require("docker-core")
 
 local function main()
+  local bin = "/usr/bin"
   core.run(
     "apk add --no-cache composer patch git $(apk search --no-cache -xq php7* | grep -Ev '(-apache2|-cgi|-dev|-doc|-pecl-ssh2|-pecl-imagick)$')"
   )
+
   -- core.run("composer self-update")
   core.run("mkdir -p /usr/local/etc/php7 /usr/local/lib/composer /var/www/html")
   core.link_log("/var/log/php7/access.log", "/var/log/php7/error.log")
@@ -13,10 +15,9 @@ local function main()
     "include = /etc/php7/docker/*.conf \n include = /usr/local/etc/php7/*.conf \n"
   )
 
-  local bin = "/usr/bin"
-  core.run("%s/composer diagnose", bin)
-  core.run("%s/composer global require hirak/prestissimo", bin)
-  core.run("%s/composer clear-cache", bin)
+  -- core.run("%s/composer diagnose", bin)
+  -- core.run("%s/composer global require hirak/prestissimo", bin)
+  -- core.run("%s/composer clear-cache", bin)
 end
 
 main()
