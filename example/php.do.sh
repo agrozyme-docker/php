d55e0e3779a99062169ae7070f5a216deef75bde
agrozyme <agrozyme@gmail.com>
2020-03-09 12:07:18 +0800

    update example

--- example/php.do.sh
+++ example/php.do.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/bash
 set -euo pipefail
 
+function source_file() {
+  echo "$(readlink -f ${BASH_SOURCE[0]})"
+}
+
+function source_path() {
+  echo "$(dirname $(source_file))"
+}
+
+function setup_alias() {
+  local run="$(source_file)"
+
+  alias php="${run} php"
+  alias composer="${run} composer"
+}
+
 function cli_command() {
   local image="docker.io/agrozyme/php"
-  local path="$(dirname $(readlink -f ${BASH_SOURCE[0]}))"
-  local command="${path}/docker.do.sh run_command -v ${PWD}:/var/www/html $@ ${image} "
+  local command="$(source_path)/docker.do.sh run_command -v ${PWD}:/var/www/html $@ ${image} "
   echo "${command}"
 }
 
